267e517f860b8ec90e97a4d20c9251011ac6f446
davidmccoy <david.mccoy@gmail.com>
2014-08-07 23:14:03 -0400

    Updated resume and refactored click event.

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,36 @@
 $(document).ready(function(){
 
-  // four click functions, one for each category
-  // toggles table row background color and slide status
-  $("tr#about").click(function(event){
+  // click event handlers for the category table rows
+  $('.category').click(function(event) {
     event.preventDefault();
-    event.stopPropagation;
+    event.stopPropagation();
 
-    var about = $("tr.about");
+    var category = $(this);
+    var childRow = $(this).attr('id');
 
-    if( $("tr#about").css("background-color") === "rgb(255, 255, 0)") {
-      $("tr#about").attr("style", "background-color: none");
-    } else {
-      $("tr#about").attr("style", "background-color: rgb(255, 255, 0)");
+    if ($(category).is($('.active'))) {
+      $('tr.' + $('tr.active').attr('id')).hide();
+      $('tr.active').removeClass('active');
+    } else if ($('tr.active')) {
+      $('tr.' + $('tr.active').attr('id')).hide();
+      $('tr.active').removeClass('active');
+
+      $(this).addClass('active');
+      $('tr.' + childRow ).show();
     }
 
-    $(about).slideToggle();
-
-  });
-
-  $("tr#technologies").click(function(event){
-    event.preventDefault();
-    event.stopPropagation;
-
-    var tech = $("tr.technologies");
-
-    if( $("tr#technologies").css("background-color") === "rgb(255, 255, 0)") {
-      $("tr#technologies").attr("style", "background-color: none");
-    } else {
-      $("tr#technologies").attr("style", "background-color: rgb(255, 255, 0)");
-    }
-
-    $(tech).slideToggle();
-  });
-
-  $("tr#portfolio").click(function(event){
-    event.preventDefault();
-    event.stopPropagation;
-
-    var portfolio = $("tr.portfolio");
-
-    if( $("tr#portfolio").css("background-color") === "rgb(255, 255, 0)") {
-      $("tr#portfolio").attr("style", "background-color: none");
-    } else {
-      $("tr#portfolio").attr("style", "background-color: rgb(255, 255, 0)");
-    }
-
-    $(portfolio).slideToggle();
-  });
-
-  $("tr#find-me").click(function(event){
-    event.preventDefault();
-    event.stopPropagation;
-
-    var findMe = $("tr.find-me");
-
-    if( $("tr#find-me").css("background-color") === "rgb(255, 255, 0)") {
-      $("tr#find-me").attr("style", "background-color: none");
-    } else {
-      $("tr#find-me").attr("style", "background-color: rgb(255, 255, 0)");
-    }
-
-    $(findMe).slideToggle();
   });
 
   // changes "full-stack web developer" to "protocol droid" on hover
   $("h1").hover(function(event) {
     event.preventDefault();
-    event.stopPropagation;
+    event.stopPropagation();
 
     $(this).attr("style", "width: 850px").text("Index of /david mccoy/protocol droid/human-cyborg relations");
 
   }, function(event) {
     event.preventDefault();
-    event.stopPropagation;
+    event.stopPropagation();
 
     $(this).attr("style", "width: 650px").text("Index of /david mccoy/full-stack web developer");
   });  
