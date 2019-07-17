b09280a01b81144de54c524d33317bbf200a70ef
David McCoy <david.mccoy@gmail.com>
2019-07-16 20:29:33 -0500

    2019 revision

--- js/title.js
+++ js/title.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
   $("#title").hover(function(e) {
     $(this).text("Index of /david mccoy/protocol droid/human-cyborg relations");
   }, function(e) {
-    $(this).text("Index of /david mccoy/full-stack web developer");
+    $(this).text("Index of /david mccoy/senior full-stack web developer");
   });
 })
