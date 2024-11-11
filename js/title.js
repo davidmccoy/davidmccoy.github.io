ac6c8d7e4672710c92104f08d1747002bd584721
David McCoy <david@Davids-MacBook-Pro.local>
2024-11-10 20:01:48 -0800

    rewrite in nextjs

--- js/title.js
+++
@@ -1,8 +0,0 @@
-// changes "full-stack web developer" to "protocol droid" on hover
-$(document).ready(function() {
-  $("#title").hover(function(e) {
-    $(this).text("Index of /david mccoy/protocol droid/human-cyborg relations");
-  }, function(e) {
-    $(this).text("Index of /david mccoy/senior full-stack web developer");
-  });
-})
