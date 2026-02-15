2996172e9e617c69b129f286cd3e6fd20e980ce4
David McCoy <david.mccoy@gmail.com>
2026-02-14 17:54:59 -0800

    tinacms build

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ cacheDir: '/Users/david/dev/personal/blog/tina/__generated__/.cache/1771120464684', url: 'https://content.tinajs.io/1.6/content/9d9d3755-835e-4818-996a-3dbb234ca839/github/main', token: '505db432cacb65cd9bd5f8a3760775f5011e29af', queries,  });
 export default client;
   
